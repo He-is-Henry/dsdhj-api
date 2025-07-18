45a3c36fd168507631d81ec14dc29d245ed27115
He-is-Henry <henries90@gmail.com>
2025-07-18 01:37:24 +0100

    Update allowList.js

--- config/allowList.js
+++ config/allowList.js
@@ -1,8 +1,8 @@
 const allowlist = [
   "http://localhost:5173",
   "127.0.0.1:5173",
-  "https://dsdhj.netlify.app/",
-  "https://www.dsdhj.netlify.app/",
+  "https://dsdhj.netlify.app",
+  "https://www.dsdhj.netlify.app",
 ];
 
 module.exports = allowlist;
