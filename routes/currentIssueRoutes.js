ca7a9b87f240ab8e35a941166e758be3a0f974c5
Henry <Henries90@gmail.com>
2025-07-18 14:35:39 +0100

    Removed .default error from controller

--- routes/currentIssueRoutes.js
+++ routes/currentIssueRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const currentIssueController =
-  require("../controllers/currentIssueController").default;
+const currentIssueController = require("../controllers/currentIssueController");
 const verifyJWT = require("../middleware/verifyJWT");
 const verifyRoles = require("../middleware/verifyRoles");
 
