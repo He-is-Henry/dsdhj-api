586cb2f6537f806773ec5806bc667e6564a2c20f
Henry <Henries90@gmail.com>
2026-04-14 09:18:54 +0100

    Updated supabase

--- routes/supabaseRoutes.js
+++ routes/supabaseRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const ctrl = require("../controllers/archiveController");
+const ctrl = require("../controllers/supabaseController");
 const multer = require("multer");
 
 const upload = multer({ storage: multer.memoryStorage() });
